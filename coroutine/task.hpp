a00177840d33d374d23831136ba15af63687a463
Ali Abbas <aliabbas.299792@gmail.com>
2023-12-21 15:58:42 +0000

    Refactored and updated tests

--- coroutine/task.hpp
+++ coroutine/task.hpp
@@ -6,10 +6,12 @@
 #include <coroutine>
 #include <iostream>
 
-struct EvTask {
+class EvTask {
+  bool started_coro{};
+
+public:
   struct promise_type;
   using Handle = std::coroutine_handle<promise_type>;
-  bool started_coro{};
 
   struct promise_type {
     struct {
@@ -33,27 +35,28 @@ struct EvTask {
 
   EvTask(Handle h) : handle(h) {}
 
-  CommunicationChannel &start() {
+  CommunicationChannel *start() {
     if (started_coro) {
       std::cerr << "The coroutine was already started, this may be discarding "
                    "some data passed via await\n";
+      return nullptr;
     }
 
     started_coro = true;
 
     auto &state = handle.promise().state;
-    auto &com_data = state.com_data;
+    auto &com_channel = state.com_data;
     handle.resume();
 
     if (state.exception_ptr) {
       std::rethrow_exception(state.exception_ptr);
     }
 
-    return com_data;
+    return &com_channel;
   }
 
   template <ResponseType RespT, typename ParamType = RespTypeMap<RespT>>
-  CommunicationChannel &resume(ParamType resp_data) {
+  CommunicationChannel *resume(ParamType resp_data) {
     if (!started_coro) {
       std::cerr << "We may be passing data to the coroutine which it won't "
                    "read since it hasn't started yet\n";
@@ -65,9 +68,9 @@ struct EvTask {
     // After the coroutine pauses again (at point X) then we may have a request
     // from it
     auto &state = handle.promise().state;
-    auto &com_data = state.com_data;
+    auto &com_channel = state.com_data;
 
-    com_data.set_resp_data<RespT>(resp_data);
+    com_channel.set_resp_data<RespT>(resp_data);
     handle.resume();
     // point X
 
@@ -75,7 +78,7 @@ struct EvTask {
       std::rethrow_exception(state.exception_ptr);
     }
 
-    return com_data;
+    return &com_channel;
   }
 
   bool is_done() { return handle.done(); }
